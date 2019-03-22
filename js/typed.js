be3ead95236453c9b490758f4052c32b82147d75
Azamat Uzbekov <batukasss@gmail.com>
2019-03-21 22:12:18 -0600

    typed js file is added

---
+++ js/typed.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typed", {
+  strings: ["Weight", "Revenue", "Cost"],
+  smartBackspace: true, // Default value
+  loop: true,
+  typeSpeed: 200,
+  backSpeed: 100,
+  smartBackspace: true,
+  backDelay: 200,
+  startDelay: 200, 
+});
